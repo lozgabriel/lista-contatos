ae9c1d86f0c4ab66e5a33714989b9bcb5db28c74
Gabriel <glozano@prpservicos.com.br>
2017-10-10 10:44:19 -0300

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,11 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
-  carro = 'Ferrari';
 
-  constructor() {
-    console.log(this.carro);
-  }
+  constructor() {}
 
 }
